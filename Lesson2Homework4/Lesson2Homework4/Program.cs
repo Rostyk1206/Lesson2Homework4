657587fe1cd7fc7196b46574cc3f11402fb87eb1
unknown <rostykvoznyak@gmail.com>
2020-10-27 23:37:44 +0200

    Lesson2Homework4

---
+++ Lesson2Homework4/Lesson2Homework4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson2Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            int r = 6;
+            double S = pi * (r*r);
+            Console.WriteLine($"Square of circle={S}");
+        }
+    }
+}
